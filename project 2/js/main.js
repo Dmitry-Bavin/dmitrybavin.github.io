5e2c38959a2bb34e44d0851fcef417de421e31b0
Dmitry-Bavin <my.work.mail.bd@yandex.ru>
2020-11-25 13:28:45 +0300

    new practice

---
+++ project 2/js/main.js
@@ -0,0 +1,13 @@
+$(function(){
+
+  $('.slider__inner').slick({
+    infinite: false,
+    prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
+    nextArrow: '<button type="button" class="slick-btn slick-next"></button>'
+    
+    
+  });
+
+  
+
+});
