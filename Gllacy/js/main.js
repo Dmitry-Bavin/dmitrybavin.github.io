519292e0def9cf648f7518905ebbbccce443600e
dmitrybavin <61393701+dmitrybavin@users.noreply.github.com>
2021-05-12 17:54:09 +0300

    trash deleted

--- Gllacy/js/main.js
+++
@@ -1,6 +0,0 @@
-let contactForm = document.querySelector("modal-contact");
-let contactBtn = document.querySelector("contacts__btn-feedback");
-
-function contactFormAction() {
-  addEventListener('onclic', contactBtn);
-}
