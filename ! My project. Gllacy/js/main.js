ab6dc8573fc67f95e15e872855a0ecbe5a52e5b7
dmitrybavin <61393701+dmitrybavin@users.noreply.github.com>
2021-05-12 06:04:54 +0300

    .
    .

---
+++ ! My project. Gllacy/js/main.js
@@ -0,0 +1,6 @@
+let contactForm = document.querySelector("modal-contact");
+let contactBtn = document.querySelector("contacts__btn-feedback");
+
+function contactFormAction() {
+  addEventListener('onclic', contactBtn);
+}
