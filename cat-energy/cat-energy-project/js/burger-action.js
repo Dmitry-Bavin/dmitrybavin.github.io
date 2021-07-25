a39b5bf540574c0d0492ecf424bdbd61ea7bc3e1
dmitrybavin <61393701+dmitrybavin@users.noreply.github.com>
2021-07-26 00:42:11 +0300

    Сделал страницу с формой
    Сделал страницу с формой
    Внёс некоторые корректировки

--- cat-energy/cat-energy-project/js/burger-action.js
+++ cat-energy/cat-energy-project/js/burger-action.js
@@ -3,7 +3,6 @@ const menuBody = document.querySelector('.nav');
 
 if (iconMenu) {
   iconMenu.addEventListener("click", function (e) {
-    document.body.classList.toggle('_lock');
     iconMenu.classList.toggle('_active');
     menuBody.classList.toggle('_active');
   });
--- cat-energy/cat-energy-project/js/script.js
+++ cat-energy/cat-energy-project/js/script.js
@@ -65,7 +65,6 @@ const menuBody = document.querySelector('.nav');
 
 if (iconMenu) {
   iconMenu.addEventListener("click", function (e) {
-    document.body.classList.toggle('_lock');
     iconMenu.classList.toggle('_active');
     menuBody.classList.toggle('_active');
   });
